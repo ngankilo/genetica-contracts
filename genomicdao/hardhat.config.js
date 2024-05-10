16c23ed85b0fc072b0ac42b3f6d0d973e60dc7e3
phamanhtan <phamanhtan@proton.me>
2024-05-10 14:50:40 +0700

    Update deploy contract script

--- genomicdao/hardhat.config.js
+++ genomicdao/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.25",
-};
+    solidity: "0.8.25",
+    networks: {
+        "optimism-sepolia": {
+            url: "https://sepolia.optimism.io",
+            chainId: 11155420,
+            accounts: [process.env.PRIVATE_KEY],
+        }
+    }
+}
